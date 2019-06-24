60a1d63db9306ae2e409615a27179e3b1b21c432
Moxuan <yuanerr@qq.com>
2019-06-24 10:30:01 +0800

    配置环境中添加了go的各种工具地址

--- HelloWorld.go
+++ HelloWorld.go
@@ -2,26 +2,13 @@ package main
 
 import (
 	"fmt"
-	"sync"
 
 	shell "github.com/ipfs/go-mefs-api"
 )
 
 func main() {
-	var test sync.Map
-	test.Store("aaa", "bbb")
-	testvalue, ok := test.Load("aaa")
-	if ok {
-		fmt.Println("testvalue:", testvalue)
-	}
-	test.Delete("a")
-	testvalue, ok = test.Load("aaa")
-	if ok {
-		fmt.Println("testvalue:", testvalue)
-	} else {
-		fmt.Println("删除成功,testvalue:", testvalue)
-	}
-
+	a := 123123 * 1024 * 1024
+	fmt.Println(a / 1024 / 1024 / 60 / 60 * 10)
 }
 
 func testRS() {
